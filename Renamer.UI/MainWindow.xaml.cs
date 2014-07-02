d5a22946dfb539d8c39167a6e4ce1b1f3a47b757
John Landheer <john@landheer.nl>
2014-07-02 07:53:26 +0200

    ViewModel

--- Renamer.UI/MainWindow.xaml.cs
+++ Renamer.UI/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -13,28 +15,110 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Renamer.UI.Annotations;
 
 namespace Renamer.UI
 {
    /// <summary>
    /// Interaction logic for MainWindow.xaml
    /// </summary>
-   public class ShowItem
+   public class ShowItem : INotifyPropertyChanged
    {
+      private string _showNameOnDisk;
+      private string _location;
+      public event PropertyChangedEventHandler PropertyChanged;
 
+      [NotifyPropertyChangedInvocator]
+      protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+      {
+         var handler = PropertyChanged;
+         if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+      }
+
+      public string ShowNameOnDisk
+      {
+         get { return _showNameOnDisk; }
+         set
+         {
+            if (value == _showNameOnDisk) return;
+            _showNameOnDisk = value;
+            OnPropertyChanged();
+         }
+      }
+
+      public string Location
+      {
+         get { return _location; }
+         set
+         {
+            if (value == _location) return;
+            _location = value;
+            OnPropertyChanged();
+         }
+      }
+
+      public ObservableCollection<EpisodeItem> Episodes { get; set; }
+   }
+
+   public class EpisodeItem : INotifyPropertyChanged
+   {
+      private string _name;
+      private int _episode;
+      private int _season;
+      public event PropertyChangedEventHandler PropertyChanged;
+
+      [NotifyPropertyChangedInvocator]
+      protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+      {
+         var handler = PropertyChanged;
+         if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+      }
+
+      public int Season
+      {
+         get { return _season; }
+         set
+         {
+            if (value == _season) return;
+            _season = value;
+            OnPropertyChanged();
+         }
+      }
+
+      public int Episode
+      {
+         get { return _episode; }
+         set
+         {
+            if (value == _episode) return;
+            _episode = value;
+            OnPropertyChanged();
+         }
+      }
+
+      public string Name
+      {
+         get { return _name; }
+         set
+         {
+            if (value == _name) return;
+            _name = value;
+            OnPropertyChanged();
+         }
+      }
    }
 
    public partial class MainWindow : Window
    {
-      private ObservableCollection<ShowDirectory> _shows;
+      private ObservableCollection<ShowItem> _shows;
 
       public MainWindow()
       {
          InitializeComponent();
-         _shows = new ObservableCollection<ShowDirectory>();
-         _shows.Add(new ShowDirectory { ShowNameOnDisk = "Breaking Bad", Location = @"S:\HD\Breaking Bad" });
-         _shows.Add(new ShowDirectory { ShowNameOnDisk = "Community", Location = @"S:\HD\Community (2007)" });
-         _shows.Add(new ShowDirectory { ShowNameOnDisk = "Homeland", Location = @"S:\HD\Homeland" });
+         _shows = new ObservableCollection<ShowItem>();
+         _shows.Add(new ShowItem { ShowNameOnDisk = "Breaking Bad", Location = @"S:\HD\Breaking Bad" });
+         _shows.Add(new ShowItem { ShowNameOnDisk = "Community", Location = @"S:\HD\Community (2007)" });
+         _shows.Add(new ShowItem { ShowNameOnDisk = "Homeland", Location = @"S:\HD\Homeland" });
       }
 
       private void Window_Loaded(object sender, RoutedEventArgs e)
